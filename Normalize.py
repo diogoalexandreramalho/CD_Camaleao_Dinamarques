0763f9d5d2a8e28b6d6d54e3b9371eb7adb8eec6
Diogo Ramalho <diogo.ramalho1998@gmail.com>
2019-11-15 01:48:30 +0000

    adds min max normalizer

--- Normalize.py
+++ Normalize.py
@@ -3,15 +3,26 @@ import numpy as np
 from pandas.plotting import register_matplotlib_converters
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import MinMaxScaler
 
 def normalization(data: pd.DataFrame):
     # I don't want to normalize the class column
     # So i save it in data_class
+    """
     data_class = data['class']
     transf = Normalizer().fit(data) # Normalize it as usual
     data = pd.DataFrame(transf.transform(data, copy=True), columns= data.columns)
     data.pop('class') # Pop the norm_class
     data['class'] = data_class # Add the right class
+    """
+
+    # MinMax Normalizer avoids negative values
+    data_class = data['class']
+    transf = MinMaxScaler().fit(data) # Normalize it as usual
+    data = pd.DataFrame(transf.transform(data), columns= data.columns)
+    data.pop('class') # Pop the norm_class
+    data['class'] = data_class # Add the right class
+
     return data
 
 def run(data: pd.DataFrame):
